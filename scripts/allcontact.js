df64f54f3406d9c29dd0e79e80ba027c4d6e651b
DEBOERAH <eniolaofuani@gmail.com>
2024-10-31 13:31:45 +0100

    Search bar now working

--- scripts/allcontact.js
+++ scripts/allcontact.js
@@ -1,6 +1,39 @@
 
 
 // funtion to display all contacts inside the local storage in allcontact.html
+let searchbarElem = document.getElementById("searchbar");
+let backbtn = document.getElementById("backbtn");
+
+backbtn.addEventListener("click", function () {
+    window.location = "/pages/index.html";
+});
+
+searchbarElem.addEventListener("input", function () {
+    let value = searchbarElem.value;
+    let contactsListElem = document.getElementById("contactsList");
+    contactsListElem.innerHTML = "";
+
+    
+    for (let i = 0; i < retrieve.length; i++) {
+        if (retrieve[i].fullName.toLowerCase().includes(value.toLowerCase())) {
+            const li = document.createElement("li");
+            li.innerHTML = ` <p> <b> Name:</b> ${retrieve[i].fullName}</p> 
+            <p> <b> Phone:</b> ${retrieve[i].phone}</p> 
+            <p> <b> Email:</b> ${retrieve[i].email}</p>
+             <button id="singlebtn">View Contact</button>
+            <button>Edit</button>
+            <hr>`;
+            contactsListElem.appendChild(li);
+        }
+        
+        
+
+        
+        
+    }
+    
+
+});
 
 const retrieve = JSON.parse(localStorage.getItem('contact'));
 function displayContact() {
